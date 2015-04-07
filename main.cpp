70079c14955bb5b4a098b34e17efe60cc01cf109
matsys <meatgit>
2015-04-07 16:52:07 +0800

    heap sort
    heap sort non-recursive version complete
    notice 3: in max_heapify, the ending condtiton is that either is not in
    heap range

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 inline int parent(int i) {
@@ -44,12 +43,54 @@ void max_heapify(int arr[],int n, int heap_size, int i) {
     }
 }
 
+void max_heapify_non_recursive(int arr[],int n, int heap_size, int i) {
+    int l = left(i);
+    int r = right(i);
+    int largest = i;
+//    notice here: loop condition is both in heap range, not either!!!
+    while(l <= heap_size && r <= heap_size) {
+        if(arr[i] < arr[l]) {
+            largest = l;
+        }
+        if(arr[largest] < arr[r]) {
+            largest = r;
+        }
+        if(largest != i) {
+            exchange(arr[i],arr[largest]);
+            i = largest;
+            l = left(i);
+            r = right(i);
+        }
+        else {
+            break;
+        }
+    }
+
+
+
+}
+
 void build_max_heap(int arr[],int n,int heap_size) {
     for(int i = (heap_size / 2); i > 0; i--) {
         max_heapify(arr,n,heap_size,i);
     }
 }
+void build_max_heap_non_recursive(int arr[],int n,int heap_size) {
+    for(int i = (heap_size / 2); i > 0; i--) {
+        max_heapify_non_recursive(arr,n,heap_size,i);
+    }
+}
 
+void heap_sort_non_recursive(int arr[],int n) {
+    int heap_size = n-1;
+    build_max_heap_non_recursive(arr,n,heap_size);
+    for(int i = heap_size; i >= 2; i--) {
+        exchange(arr[1],arr[i]);
+        heap_size--;
+//         notice here, the start node is always the 1-st one.
+        max_heapify_non_recursive(arr,n,heap_size,1);
+    }
+}
 void heap_sort(int arr[],int n) {
     int heap_size = n-1;
     build_max_heap(arr,n,heap_size);
@@ -67,10 +108,24 @@ int main() {
     int m = 11;
     // heap star at 1, and arr[0] is dummy, may be it can be used for store the heap size
     int arr[] = {-1,1,2,4,8,91,9,3,7,10,14};
-    heap_sort(arr,m);
-//    max_heapify(arr,m,7,1);
-//    build_max_heap(arr,m,m-1);
+    int arr2[m];
+    for(int i = 0; i < m; i++) {
+        arr2[i] = arr[i];
+    }
+    cout << "original array: \n";
     print(arr,m);
+    print(arr2,m);
+    heap_sort(arr,m);
+//    max_heapify(arr,m,m-1,3);
+//    build_max_heap(arr,m,m-1);
+    cout << "recursive : \n";
+    print(arr,m);
+
+//    max_heapify_non_recursive(arr2,m,m-1,3);
+//    build_max_heap(arr,m,m-1);
+    heap_sort_non_recursive(arr2,m);
+    cout << "non-recursive : \n";
+    print(arr2,m);
 
     return 0;
 }
